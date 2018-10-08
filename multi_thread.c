28038a9bb8448ba79d59ebb95eff4717d1be2c30
keelin <keelin@pyrite-n2.cs.iastate.edu>
2018-10-07 21:02:06 -0500

    adding files

--- multi_thread.c
+++ multi_thread.c
@@ -2,106 +2,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NUM_THREADS 3
+#define NUM_THREADS 2
 
-int avg, max, min;
-void* do_avg(void* param);
-void* do_max(void* param);
-void* do_min(void* param);
+int numbers[20];
+void* write_odd();
+void* write_even();
+pthread_mutex_t mutex;
 
 int main(int argc, char* argv[])
 {
+  int i;
   pthread_t workers[NUM_THREADS];
   pthread_attr_t attr;
 
-  if (argc < 2) {
-    fprintf(stderr, "usage: a.out <interger values>\n");
-    return -1; 
-  }
-
   pthread_attr_init(&attr); /* Get the default attributes */
+  pthread_mutex_init(&mutex, NULL); /* Create mutex lock */
+
   /* Create the threads for each operation */
-  pthread_create(&workers[0], &attr, do_avg, argv);
-  pthread_create(&workers[1], &attr, do_max, argv);
-  pthread_create(&workers[2], &attr, do_min, argv);
+  pthread_create(&workers[0], &attr, write_odd, NULL);
+  pthread_create(&workers[1], &attr, write_even, NULL);
 
   /* Wait for all threads to terminate */
-  for (int i = 0; i < 3; i++)
-    pthread_join(workers[i], NULL);
-
+  pthread_join(workers[0], NULL);
+  pthread_join(workers[1], NULL);
+  
   /* Output the results */
-  printf("The average value is %d\n", avg);
-  printf("The minimum value is %d\n", min);
-  printf("The maximum value is %d\n", max);
+  for (i = 0; i < 20; i++) {
+    printf("%5d", numbers[i]);
+  }
+  printf("\n");
+
+  return 0; 
 }
 
 /* Function Implmentations */
-void* do_avg(void* param){
+void* write_odd(){
 
-  int i, num_elem;
-  char** arguments;
-  avg = 0;
-  num_elem = 0;
+  int i;
   
-  /* Convert the void* to char* */
-  arguments = param;
-  
-  /* Start at argv[1] */
-  i = 1; 
-  while (arguments[i] != NULL) {
-    avg += atoi(arguments[i]);
-    num_elem ++;
-    i ++;
-  }
-
-  avg /= num_elem;
-
-  pthread_exit(0);
-}
-
-void* do_max(void* param){
-
-  int i, n;
-  char** arguments;
-  max = 0;
-  
-  /* Convert the void* to char* */
-  arguments = param;
-  
-  /* Start at argv[1] */
-  i = 1; 
-  max = atoi(arguments[i]);
-  i ++;
-  while (arguments[i] != NULL) {
-    if ((n = atoi(arguments[i])) > max)
-      max = n;
-    i ++;
-  }
-
-  pthread_exit(0);
-}
-
-void* do_min(void* param){
- 
-  int i, n;
-  char** arguments;
-  min = 0;
-  
-  /* Convert the void* to char* */
-  arguments = param;
-  
-  /* Start at argv[1] */
-  i = 1; 
-  min = atoi(arguments[i]);
-  i ++;
-  while (arguments[i] != NULL) {
-    if ((n = atoi(arguments[i])) < min)
-      min = n;
-    i ++;
+  for (i = 0; i < 20; i += 2) {
+    pthread_mutex_lock(&mutex);
+    numbers[i] = i+1;
+    pthread_mutex_unlock(&mutex);
   }
   
   pthread_exit(0);
 }
+
+void* write_even(){
+
+  int i;
+  
+  for (i = 1; i < 20; i += 2) {
+    pthread_mutex_lock(&mutex);
+    numbers[i] = i+1;
+    pthread_mutex_unlock(&mutex);
+  }
+  
+  pthread_exit(0);
+}
+
 
 
 
